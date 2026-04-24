35f26d7e6cea8522bab88b40d05831d7fea967de
VadmanBat <VabmanBat@gmail.com>
2026-04-24 22:29:14 +0300

    PolySolver: cpp -> hpp

--- numina/classes/poly-solver/include/numina/poly-solver.h
+++ numina/classes/poly-solver/include/numina/poly-solver.h
@@ -85,29 +85,9 @@ private:
 
     std::vector<Complex> extract_answer() noexcept;
 };
-
-inline PolySolver::Complex PolySolver::f(const Complex& x) const noexcept {
-    Complex result = c[0];
-    for (std::size_t i = 1; i <= degree; ++i)
-        (result *= x) += c[i];
-    return result;
 }
 
-inline PolySolver::Complex PolySolver::d1(const Complex& x) const noexcept {
-    Complex result = c_d1[0];
-    for (std::size_t i = 1; i < degree; ++i)
-        (result *= x) += c_d1[i];
-    return result;
-}
-
-inline PolySolver::Complex PolySolver::d2(const Complex& x) const noexcept {
-    const auto n   = degree - 1;
-    Complex result = c_d2[0];
-    for (std::size_t i = 1; i < n; ++i)
-        (result *= x) += c_d2[i];
-    return result;
-}
-}
+#include "../../src/core/evaluate.hpp"
 
 /*
  * 1. Разделить производную на m!
--- numina/classes/poly-solver/src/core/evaluate.hpp
+++ numina/classes/poly-solver/src/core/evaluate.hpp
@@ -1,27 +1,25 @@
-#include "numina/poly-solver.h"
+#pragma once
 // Created by Vadim on 13.07.2025.
-/*
 namespace numina {
-PolySolver::Complex PolySolver::f(const Complex& x) const {
+inline PolySolver::Complex PolySolver::f(const Complex& x) const noexcept {
     Complex result = c[0];
     for (std::size_t i = 1; i <= degree; ++i)
         (result *= x) += c[i];
     return result;
 }
 
-PolySolver::Complex PolySolver::d1(const Complex& x) const {
+inline PolySolver::Complex PolySolver::d1(const Complex& x) const noexcept {
     Complex result = c_d1[0];
     for (std::size_t i = 1; i < degree; ++i)
         (result *= x) += c_d1[i];
     return result;
 }
 
-PolySolver::Complex PolySolver::d2(const Complex& x) const {
-    const auto n = degree - 1;
+inline PolySolver::Complex PolySolver::d2(const Complex& x) const noexcept {
+    const auto n   = degree - 1;
     Complex result = c_d2[0];
     for (std::size_t i = 1; i < n; ++i)
         (result *= x) += c_d2[i];
     return result;
 }
 }
-*/
